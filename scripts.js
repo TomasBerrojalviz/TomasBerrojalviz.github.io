aa3d9feca3bd50c6b485f158f04b134e1d6c2906
tomasberrojalviz <cai.berrojalviz.tomas@gmail.com>
2022-09-23 22:02:09 -0300

    Se agrega resultado texto

--- scripts.js
+++ scripts.js
@@ -4,78 +4,42 @@ const cube2 = document.querySelector('.cube2');
 const cube3 = document.querySelector('.cube3');
 const time = 2;
 element = document.getElementById('partes_topy');
-element.style.display = 'none';  
-/*
+element.style.display = 'none';
+var dadoAccion = "";
+var dadoTopy = "";
+var dadoGia = "";
+var dadoTiempo = "";
+var resultado = ""; 
 
-cube.addEventListener('click', () => {
+
+// 1 6 3 4 5 2
+
+var tiempo=["por 30 segundos","hasta acabar","por 1 minutos","por 2 minutos","cuanto desee","por 45 segundos"];
+var partes_topy=["la pija","el pelo","el cuello","el torso","la boca","los huevos"];
+var partes_gia=["la concha","la boca","el culo","las piernas","el pelo","las tetas"];
+var accion=["Chupar","✨69✨","Dar besos en","Acariciar","Lamer","Pajear/Masajear"];
+
+function tirarDado(cube, nro){
     cube.style.transition = '';
     cube.style.transform = `translateY(200px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
     setTimeout(() => {
         cube.style.transition = `transform ${time}s`;
         const randomValue = Math.floor((Math.random() * 6) + 1);
-        console.log(`randomValue: ${randomValue}` );
-        
-        switch(randomValue) {
-            case 1:             
-                cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
-                break;
+        switch(nro){
+            case 0:
+                dadoAccion = accion[randomValue-1];
+            break;
+            case 1:
+                dadoTopy = partes_topy[randomValue-1];
+            break;
             case 2:
-                cube.style.transform = `translateY(200px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
-                break;
+                dadoGia = partes_gia[randomValue-1];
+            break;
             case 3:
-                cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
-                break;
-            case 4:
-                cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
-                break;
-            case 5:
-                cube.style.transform = `translateY(200px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
-                break;
-            case 6:
-                cube.style.transform = `translateY(200px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
-                break;
-        };
-    }, time * 10);
-
-});
-cube2.addEventListener('click', () => {
-    cube2.style.transition = '';
-    cube2.style.transform = `translateY(200px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
-    setTimeout(() => {
-        cube2.style.transition = `transform ${time}s`;
-        const randomValue = Math.floor((Math.random() * 6) + 1);
-        console.log(`randomValue: ${randomValue}` );
-        
-        switch(randomValue) {
-            case 1:             
-                cube2.style.transform = `translateY(200px) rotateX(3600deg) rotateY(3600deg) rotateZ(3600deg)`;
-                break;
-            case 2:
-                cube2.style.transform = `translateY(200px) rotateX(4410deg) rotateY(3600deg) rotateZ(3600deg)`;
-                break;
-            case 3:
-                cube2.style.transform = `translateY(200px) rotateX(3600deg) rotateY(4410deg) rotateZ(3600deg)`;
-                break;
-            case 4:
-                cube2.style.transform = `translateY(200px) rotateX(3600deg) rotateY(2430deg) rotateZ(3600deg)`;
-                break;
-            case 5:
-                cube2.style.transform = `translateY(200px) rotateX(2430deg) rotateY(3600deg) rotateZ(3600deg)`;
-                break;
-            case 6:
-                cube2.style.transform = `translateY(200px) rotateX(3600deg) rotateY(1980deg) rotateZ(3600deg)`;
-                break;
-        };
-    }, time * 10);
-
-});
-*/
-function tirarDado(cube){
-    cube.style.transition = '';
-    cube.style.transform = `translateY(200px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
-    setTimeout(() => {
-        cube.style.transition = `transform ${time}s`;
-        const randomValue = Math.floor((Math.random() * 6) + 1);
+                dadoTiempo = tiempo[randomValue-1];
+            break;
+        }
+        console.log("nro dado: " + nro );
         console.log(`randomValue: ${randomValue}` );
         
         switch(randomValue) {
@@ -101,24 +65,36 @@ function tirarDado(cube){
     }, time * 10);
 
 }
-var tiempo=["por 2 minutos","por 5 minutos","por 7 minutos","por 10 minutos","hasta que la persona quiera","hasta acabar"]
-var partes_topy=["la pija","los huevos","el cuello","el torso","la boca","el pelo"]
-var partes_gia=["la concha","las tetas","el culo","las piernas","el pelo","la boca"]
-var accion=["Chupar","Pajear/Masajear","Dar besos en","Acariciar","Lamer","✨69✨"]
-var texto='';  
+
 function juego2(gia) {
-	if(!gia){
-		mostrar('partes_topy');
-		ocultar('partes_gia');
-	}
-	else{
-		ocultar('partes_topy');
-		mostrar('partes_gia');
-	}
-	tirarDado(cube);
-	tirarDado(cube1);
-	tirarDado(cube2);
-	tirarDado(cube3);
+	tirarDado(cube, 0);
+        if(!gia){
+            mostrar('partes_topy');
+            ocultar('partes_gia');
+            tirarDado(cube1, 1);
+        }
+        else{
+            ocultar('partes_topy');
+            mostrar('partes_gia');
+            tirarDado(cube2, 2);
+        }
+        tirarDado(cube3, 3);
+    
+    setTimeout(() => {
+        resultado = dadoAccion;
+        if(dadoAccion != accion[1]){
+            if(!gia){
+                resultado += " " + dadoTopy;
+            }
+            else{
+                resultado += " " + dadoGia;
+            }
+            resultado += " " + dadoTiempo;
+        }
+        console.log(resultado);
+        document.getElementById("salida").innerHTML = '<b>'+resultado+'</b>';
+    }, 1800);
+
 }
 function juego(quien) {
 	var resultado=''; 
@@ -157,9 +133,14 @@ function agregartxt2(){
 }   
 
 function ocultar(id){
-document.getElementById(id).style.display = 'none';
+    document.getElementById(id).style.display = 'none';
 }
 
 function mostrar(id){
-document.getElementById(id).style.display = 'block';
+    document.getElementById(id).style.display = 'block';
 }
+
+function resultado(){
+    document.getElementById(id).style.display = 'block';
+}
+            
